769bafad76ebca8cb50f1c8d02fad6d7861ec3e9
kevin xirum <kevinmenjivar66@gmail.com>
2020-05-16 22:11:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,10 +9,10 @@ end
 
 
 def reverse_each_word(string)
-  array = sentence.split ("")
+  array = string.split ("")
   new_array = []
   array.collect do |string|
-    new_array << sentence.reverse
+    new_array << string.reverse
   end
   new_array.join ("")
 end
