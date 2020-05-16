d3fd9312535141fe52575f2e0b66cf470da5b229
kevin xirum <kevinmenjivar66@gmail.com>
2020-05-16 22:07:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   array.collect do |sentence|
     new_array << sentence.reverse
   end
-  new_array.join ("")
+  new_array.join (" ")
 end
 
 reverse_each_word("Hello there, and how are you?")
